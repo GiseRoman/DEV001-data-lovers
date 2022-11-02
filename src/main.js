3a6447ddc8d02419e3ffdf02e971b7c7adf76ea7
Carolina Pacori <carolina.pacori@gmail.com>
2022-11-02 18:12:22 -0500

    el real comienzo:)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
 console.log(example, data);
+
+
+const container = document.getElementById('container');
+data.films.forEach(film =>{
+    container.insertAdjacentHTML('beforeend', `<div class="cajitas">  <img class="posters" src="${film.poster}" width="157" height="202"></img><p class="titulos">${film.title}</p></div>`)
+    });
+        
