6c866719c82edd5bef871809e15170014f444765
Gise <gisenoctis@gmail.com>
2022-11-02 17:17:54 -0600

    ignorar

--- src/data.js
+++ src/data.js
@@ -1,11 +1,17 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+// import data from "./data/ghibli/ghibli";
 
+// let peliculas=[];
+// for (var i=0;i<data.films.length; i++){
+// peliculas.push(data.films[i].title)
+// }
+// document.getElementById("nombres").innerHTML = peliculas
 
