cdfd690bf2b8417ee8db9d0b9910a16a87371624
Gise <gisenoctis@gmail.com>
2022-11-02 17:45:08 -0600

    diseño tarjetas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { example } from './data.js';
+// import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
+// console.log(example, data);
 
 
 const container = document.getElementById('container');
 data.films.forEach(film =>{
-    container.insertAdjacentHTML('beforeend', `<div class="cajitas">  <img class="posters" src="${film.poster}" width="157" height="202"></img><p class="titulos">${film.title}</p></div>`)
+    container.insertAdjacentHTML('beforeend', `<div class="cajitas"> <img class="posters" src="${film.poster}" width="157" height="202"></img><p class="titulos">${film.title}</p></div>`)
     });
