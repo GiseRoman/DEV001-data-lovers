62ca68d23582c3da0e8f7105492659862dc624ae
Gise <gisenoctis@gmail.com>
2022-11-02 23:54:02 -0600

    ignorame

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import data from './data/ghibli/ghibli.js';
 // console.log(example, data);
 
 
-const container = document.getElementById('container');
+const container = document.getElementById('tarjeta');
+
 data.films.forEach(film =>{
-    container.insertAdjacentHTML('beforeend', `<div class="cajitas"> <img class="posters" src="${film.poster}" width="157" height="202"></img><p class="titulos">${film.title}</p></div>`)
+    const peliculas = `<div class="container"> <div class="cajitasFrente"><img class="posters" src="${film.poster}" width="157" height="202"></img><p class="titulos">${film.title}</p></div></div> <div class="cajitasAtras"><p class="textos">${film.description}"></p><p class="textos">Director: ${film.director}</p><p class="textos">Producer: ${film.producer}</p><p class="textos"> Year: ${film.release_date}</p><p class="textos tarjetas">Score: ${film.rt_score}</p></div></div>`
+    container.insertAdjacentHTML('beforeend', peliculas)
     });
+
+// data.films.forEach(film =>{
+//     container.insertAdjacentHTML('beforeend', `<div id="container"><div class="cajitasAtras"> <p class="textos">${film.description}"></p><p class="textos">${film.director}</p><p class="textos">${film.producer}</p><p class="textos">${film.release_date}</p><p class="textos">${film.rt_score}</p></div></div></div>`)
+//     });
