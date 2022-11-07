a01af5aa92c49de55ac98e477b278104f4906b5c
Gise <gisenoctis@gmail.com>
2022-11-07 08:37:52 -0600

    ocultar info cajita-atras

--- src/main.js
+++ src/main.js
@@ -49,14 +49,14 @@ data.films.forEach(film => {
     peoples.forEach(personajes =>{
         const personaje = `<div class="container"><div class="cajitasFrente">
         <img class="posters" src="${personajes.img}" width="157" height="202"></img>
-        <p class="titulos">${personajes.name}</p></div></div>
-        <div class = "cajitasAtras">
-        <p class = "textos">Movie: ${film.title}</p>
-        <p class = "textos">Gender: ${personajes.gender}</p>
-        <p class = "textos">Age: ${personajes.age}</p>
-        <p class = "textos">Eye color: ${personajes.eye_color}</p>
-        <p class = "textos">Hair color: ${personajes.hair_color}</p>
-        <p class = "textos">Specie: ${personajes.specie}</p></div>`
+        <p class="titulos">${personajes.name}</p></div></div>`
+        // <div class = "cajitasAtras">
+        // <p class = "textos">Movie: ${film.title}</p>
+        // <p class = "textos">Gender: ${personajes.gender}</p>
+        // <p class = "textos">Age: ${personajes.age}</p>
+        // <p class = "textos">Eye color: ${personajes.eye_color}</p>
+        // <p class = "textos">Hair color: ${personajes.hair_color}</p>
+        // <p class = "textos">Specie: ${personajes.specie}</p></div>
         peopleContainer.insertAdjacentHTML('beforeend', personaje)
     })
 })
@@ -80,13 +80,13 @@ data.films.forEach(film =>{
     location.forEach(locations =>{
         const locaciones = `<div class="container"><div class="cajitasFrente">
         <img class="posters" src="${locations.img}" width="157" height="202"></img>
-        <p class="titulos">${locations.name}</p></div></div>
-        <div class = "cajitasAtras">
-        <p class = "textos">Movie: ${film.title}</p>
-        <p class = "textos">Climate: ${locations.climate}</p>
-        <p class = "textos">Terrain: ${locations.terrain}</p>
-        <p class = "textos">Residents: ${locations.residents}</p>
-        <p class = "textos">Surface water: ${locations.surface_water}</p></div>`
+        <p class="titulos">${locations.name}</p></div></div>`
+        // <div class = "cajitasAtras">
+        // <p class = "textos">Movie: ${film.title}</p>
+        // <p class = "textos">Climate: ${locations.climate}</p>
+        // <p class = "textos">Terrain: ${locations.terrain}</p>
+        // <p class = "textos">Residents: ${locations.residents}</p>
+        // <p class = "textos">Surface water: ${locations.surface_water}</p></div>
         locationsContainer.insertAdjacentHTML('beforeend', locaciones)
     })
 })
@@ -110,13 +110,13 @@ data.films.forEach(film =>{
     vehiculos.forEach(vehicle =>{
         const vehicles = `<div class="container"><div class="cajitasFrente">
         <img class="posters" src="${vehicle.img}" width="157" height="202"></img>
-        <p class="titulos">${vehicle.name}</p></div></div>
-        <div class = "cajitasAtras">
-        <p class = "textos">Movie: ${film.title}</p>
-        <p class = "textos">Description: ${vehicle.description}</p>
-        <p class = "textos">Class: ${vehicle.vehicle_class}</p>
-        <p class = "textos">Lenght: ${vehicle.length}</p>
-        <p class = "textos">Pilot: ${vehicle.pilot.name}</p></div>`
+        <p class="titulos">${vehicle.name}</p></div></div>`
+        // <div class = "cajitasAtras">
+        // <p class = "textos">Movie: ${film.title}</p>
+        // <p class = "textos">Description: ${vehicle.description}</p>
+        // <p class = "textos">Class: ${vehicle.vehicle_class}</p>
+        // <p class = "textos">Lenght: ${vehicle.length}</p>
+        // <p class = "textos">Pilot: ${vehicle.pilot.name}</p></div>
         vehiclesContainer.insertAdjacentHTML('beforeend', vehicles)
     })
 })
