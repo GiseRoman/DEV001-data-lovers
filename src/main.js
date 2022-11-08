46c56fd7da2d1d68991cb5e9f95ec39936c5f20f
Carolina Pacori <carolina.pacori@gmail.com>
2022-11-08 11:10:06 -0500

    sin cambios

--- src/main.js
+++ src/main.js
@@ -31,16 +31,16 @@ const movieContainer = document.getElementById('peliculas')
 //mostrar data de peliculas
 data.films.forEach(film =>{
     const peliculas = `<div class="container"> <div class="cajitasFrente">
+    <span class="close">&times;</span>
     <img class="posters" src="${film.poster}" width="157" height="202"></img>
     <p class="titulos">${film.title}</p></div></div>`
-    // <div class="cajitasAtras"><p class="textos">${film.description}"</p>
+    // <div class ="cajitasAtras"><p class="textos">${film.description}"</p>
     // <p class="textos">Director: ${film.director}</p>
     // <p class="textos">Producer: ${film.producer}</p>
     // <p class="textos"> Year: ${film.release_date}</p>
     // <p class="textos tarjetas">Score: ${film.rt_score}</p></div></div>
     movieContainer.insertAdjacentHTML('beforeend', peliculas)
     });
-
 
 //llamar a la data desde el boton Character
 const btn2 = document.getElementById("personaje")
