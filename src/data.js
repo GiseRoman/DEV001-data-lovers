a1f0811f6e8d23597f0003063b21a698002a7da9
Gisela Roman <gisenoctis@gmail.com>
2022-11-17 13:31:17 -0600

    ordenamiento AZ funcional

--- src/data.js
+++ src/data.js
@@ -5,13 +5,24 @@ export const personajes = data.films.map(film => film.people)
 export const lugares = data.films.map(film => film.locations)
 export const vehiculos = data.films.map(film => film.vehicles)
 
-export const organizarAZ = () => {
-    data.films.sort((a,b) => {
-        return (a.title.toLowerCase() < b.title.toLowerCase()) ? -1
-        : (a.title.toLowerCase() > b.title.toLowerCase()) ? 1
-        : 0
-    })
+export const organizarAZ = (array, property) => {
+    return array.flat().sort(function (a, b) {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      });
+      
+    //  return array.sort((a,b) => {
+    //     return (a.property.toLowerCase() < b.property.toLowerCase()) ? -1
+    //     : (a.property.toLowerCase() > b.property.toLowerCase()) ? 1
+    //     : 0
+    // })
 }
+
 
 
 // export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -20,12 +20,45 @@ const descripcion = document.getElementById("descripcion")
 descripcion.insertAdjacentHTML('beforeend', `<p> Studio Ghibli was founded by manga animator, director, producer, screenwriter, author, and artist Hayao Miyazaki; Japanese film director Isao Takahata; and producer Toshio Suzuki. Takahata and Miyazaki met in the 1960s when they were both working for the Japanese animation studio Tôei Dôga. Although they collaborated for more than a decade, it was not until the mid-1980s that they decided to start their own company. In 1985, with funding from publisher Tokuma Shoten, Takahata, Miyazaki, and Suzuki founded Studio Ghibli, a small production studio in suburban Tokyo.
 </p>`)
 
+
+
 // <-----Inicio Vaciar contenedor principal-----
 const cleanContainer = () => {
     allContainer.innerHTML = ''
     descripcion.innerHTML = ''
 }
 // <-----Fin Vaciar contenedor principal-----
+
+btnOrdenAZ.addEventListener('click', () => {
+    const filterSelected = allContainer.getAttribute("filter")
+    console.log(filterSelected)
+    if(filterSelected === "characters"){
+        const sortedCharacter = organizarAZ(personajes, "name")
+        cleanContainer()
+        sortedCharacter.forEach(character =>{ 
+            allContainer.insertAdjacentHTML('beforeend', `<div class="container" id="contenedor"> <div class="cajitasFrente">
+            <img class="posters" src="${character.img}" width="157" height="202"></img>
+            <p class="titulos">${character.name}</p></div></div>`)
+        })
+    }else if(filterSelected === "locations"){
+        const sortedLocation = organizarAZ(lugares, "name")
+        cleanContainer()
+        sortedLocation.forEach(location =>{ 
+            allContainer.insertAdjacentHTML('beforeend', `<div class="container" id="contenedor"> <div class="cajitasFrente">
+            <img class="posters" src="${location.img}" width="157" height="202"></img>
+            <p class="titulos">${location.name}</p></div></div>`)
+        })
+    }else if(filterSelected === "vehicles"){
+        const sortedVehicle = organizarAZ(lugares, "name")
+        cleanContainer()
+        sortedVehicle.forEach(vehicle =>{ 
+            allContainer.insertAdjacentHTML('beforeend', `<div class="container" id="contenedor"> <div class="cajitasFrente">
+            <img class="posters" src="${vehicle.img}" width="157" height="202"></img>
+            <p class="titulos">${vehicle.name}</p></div></div>`)
+        })}
+})
+
+
 const buscar = (e => {
     searchHTMLarr = []
     cleanContainer()
@@ -66,19 +99,19 @@ console.log(searchHTMLarr)
 btnSearch.addEventListener('click',() => {cleanContainer()
     allContainer.insertAdjacentHTML('beforeend', searchHTMLarr)})
 
-
 // <-----Fin Buscador-----
 
 
 // <-----Inicio mostrar peliculas-----
 data.films.forEach(film => {
-    // organizarAZ()
     filmHTMLarr.push( `<div class="container" id="contenedor"> <div class="cajitasFrente">
     <img class="posters" src="${film.poster}" width="157" height="202"></img>
     <p class="titulos">${film.title}</p></div></div>`
 )
 })
-btn1.addEventListener('click', () => {cleanContainer()
+btn1.addEventListener('click', () => {
+    allContainer.setAttribute("filter", "movies")
+    cleanContainer()
     allContainer.insertAdjacentHTML('beforeend', `<div class="titulosAll">Peliculas de Studio Ghibli</div>`)
     allContainer.insertAdjacentHTML('beforeend', filmHTMLarr)})
 // <-----Fin mostrar peliculas-----
@@ -91,7 +124,9 @@ personajes.forEach(persons => {
     <img class="posters" src="${person.img}" width="157" height="202"></img>
     <p class="titulos">${person.name}</p></div></div>`)
 })})
-btn2.addEventListener('click', ()=>{cleanContainer() 
+btn2.addEventListener('click', ()=>{
+    allContainer.setAttribute("filter", "characters")
+    cleanContainer() 
     allContainer.insertAdjacentHTML('beforeend', `<div class="titulosAll" >Personajes de Studio Ghibli</div>`)
     allContainer.insertAdjacentHTML('beforeend',peopleHTMLarr)})
 // <-----Fin mostrar personajes-----
@@ -104,7 +139,9 @@ lugares.forEach(locaciones => {
     <img class="posters" src="${locacion.img}" width="157" height="202"></img>
     <p class="titulos">${locacion.name}</p></div></div>`)
 })})
-btn3.addEventListener('click', ()=>{cleanContainer() 
+btn3.addEventListener('click', ()=>{
+    allContainer.setAttribute("filter", "locations")
+    cleanContainer() 
     allContainer.insertAdjacentHTML('beforeend', `<div class="titulosAll" >Locaciones de Studio Ghibli</div>`)
     allContainer.insertAdjacentHTML('beforeend',locationHTMLarr)})
 // <-----Fin mostrar luares-----
@@ -117,7 +154,9 @@ vehiculos.forEach(vehicle => {
     <img class="posters" src="${vehiculo.img}" width="157" height="202"></img>
     <p class="titulos">${vehiculo.name}</p></div></div>`)
 })})
-btn4.addEventListener('click', ()=>{cleanContainer() 
+btn4.addEventListener('click', ()=>{
+    allContainer.setAttribute("filter", "vehicles")
+    cleanContainer() 
     allContainer.insertAdjacentHTML('beforeend', `<div class="titulosAll" >Vehiculos de Studio Ghibli</div>`)
     allContainer.insertAdjacentHTML('beforeend',vehicleHTMLarr)})
 // <-----Fin mostrar vehiculos-----
